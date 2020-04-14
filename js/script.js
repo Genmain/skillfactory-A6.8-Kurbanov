793fb734eb9bff327c6c54d28156171807f18e71
genmain.sw@gmail.com <genmain.sw@gmail.com>
2020-04-14 22:48:23 +0500

    modified js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
-$('button').click( function (p){
-    const pb = $('.progress-bar');
-    let percent = pb.width() / pb.parent().width() *100;
-    const delta = Number(p.target.innerText.substr(1,p.target.innerText.length-2))
-    percent = percent - (percent %1);
-    pb.width(percent+delta+"%");
-    // console.log("percent = ", percent);
-    // console.log("delta = ", delta);
-    // console.log('perc+delta+"%"', percent+delta+"%");
+var percent = 0;
+$('button').click(function (p) {
+    percent += Number(p.target.innerText.substr(1, p.target.innerText.length - 2));
+    $('.progress-bar').width(percent + "%")
 });
